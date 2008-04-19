e4cd6afc02edc26d3737dd79e70f74ead0144315
thomas.wieger <thomas.wieger@gmx.de>
2008-04-19 22:50:14 +0000

    build project can build buildng if launched from eclipse.

--- buildng/src/main/java/org/buildng/model/Library.java
+++ buildng/src/main/java/org/buildng/model/Library.java
@@ -1,20 +1,31 @@
 package org.buildng.model;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.log4j.Logger;
 
 
 public class Library {
 
     // --------------------------------------------------------------------------
+    // class variables
+    // --------------------------------------------------------------------------
+
+    private static final Logger LOG = Logger.getLogger(Library.class);
+
+
+
+    // --------------------------------------------------------------------------
     // instance variables
     // --------------------------------------------------------------------------
 
-    private String fGroupId;
-    private String fName;
-    private String fVersion;
-    private String fExtension;
+    private String              fGroupId;
+    private String              fName;
+    private String              fVersion;
+    private String              fSecondaryName;
+    private String              fExtension;
 
 
 
@@ -23,11 +34,17 @@ public class Library {
     // --------------------------------------------------------------------------
 
     public Library(String pGroupId, String pName, String pVersion, String pExtension) {
-        fGroupId   = pGroupId;
-        fName      = pName;
-        fVersion   = pVersion;
-        fExtension = pExtension;
+        this(pGroupId, pName, pVersion, null, pExtension);
     }
+    
+    public Library(String pGroupId, String pName, String pVersion, String pSecondaryName, String pExtension) {
+        fGroupId       = pGroupId;
+        fName          = pName;
+        fVersion       = pVersion;
+        fSecondaryName = pSecondaryName;
+        fExtension     = pExtension;
+    }
+
 
 
 
@@ -36,8 +53,21 @@ public class Library {
     // --------------------------------------------------------------------------
 
     public File getFile(File pRepositoryDir) {
-        return new File(pRepositoryDir, fGroupId.replace('.', File.separatorChar) + File.separator + fName
-                + File.separator + fVersion + File.separator + fName + "-" + fVersion + "." + fExtension);
+        String pathToArtifact = fGroupId.replace('.', File.separatorChar) + File.separator + fName
+                                       + File.separator + fVersion;
+        File jarFile;
+        if (fSecondaryName == null) {
+            jarFile = new File(pRepositoryDir, pathToArtifact + File.separator + fName + "-" + fVersion + "." + fExtension);
+        } else {
+            jarFile = new File(pRepositoryDir, pathToArtifact + File.separator + fSecondaryName + "-" + fVersion + "." + fExtension);
+        }
+        try {
+            LOG.debug("jarfile path=" + jarFile.getCanonicalPath());
+        } catch (IOException ex) {
+            // intentionally left blank. this is a debug statement. if it fails,
+            // we don't care!
+        }
+        return jarFile;
     }
 
 
