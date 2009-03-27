e24412ff657a4877b724bf40ff245ae1a441377c
thomas.wieger@gmx.de <thomas.wieger@gmx.de@b8d51c49-db4a-0410-8e88-ed5ddfa0639c>
2009-03-27 10:49:07 +0000

    trying to split the build...

---
+++ trunk/buildng/src/main/java/org/buildng/buildlets/Setup.java
@@ -0,0 +1,9 @@
+package org.buildng.buildlets;
+
+import org.buildng.model.Model;
+import org.buildng.model.Project;
+
+
+public interface Setup {
+    void setup(Model pModel, Project pProject);
+}
